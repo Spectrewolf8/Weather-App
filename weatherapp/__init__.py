6b163496b4f7fe53746d38e75e0fc16d4b392c3d
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2024-02-24 20:18:57 +0500

    Refactor weather data retrieval functions to handle errors

--- weatherapp/__init__.py
+++ weatherapp/__init__.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
-
+from requests.exceptions import JSONDecodeError
 
 load_dotenv()
 
@@ -10,24 +10,51 @@ load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 
 
-# Weather checking module
 def get_weather_data(query):
     api_key = os.environ.get("API_KEY")
     base_url = "http://api.weatherapi.com/v1/current.json"
     params = {"key": api_key, "q": query, "aqi": "yes"}
-    response = requests.get(base_url, params=params).json()
 
-    return response
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raises a HTTPError if the response status is 4xx, 5xx
+        data = response.json()
+
+        # Check if 'current' key exists in the response
+        if "current" in data:
+            return data
+        else:
+            print("The 'current' key does not exist in the response")
+    except JSONDecodeError:
+        print("Error decoding the response")
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def get_forecast_astro_data(query, days):
     api_key = os.environ.get("API_KEY")
     base_url = "http://api.weatherapi.com/v1/forecast.json"
     params = {"key": api_key, "q": query, "days": days}
-    response = requests.get(base_url, params=params).json()
 
-    # Extract the astro data for each day
-    astro_data = [day["astro"] for day in response["forecast"]["forecastday"]]
-    astro_data = astro_data[0]
-    print("astro_data:", astro_data)
-    return astro_data
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raises a HTTPError if the response status is 4xx, 5xx
+        data = response.json()
+
+        # Check if 'forecast' key exists in the response
+        if "forecast" in data and "forecastday" in data["forecast"]:
+            # Extract the astro data for each day
+            astro_data = [day["astro"] for day in data["forecast"]["forecastday"]]
+            astro_data = astro_data[0]
+            # print("astro_data:", astro_data)
+            return astro_data
+        else:
+            print("The 'forecast' key does not exist in the response")
+    except JSONDecodeError:
+        print("Error decoding the response")
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
