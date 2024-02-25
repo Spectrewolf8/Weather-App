0c77fa7cb4df8d70b2003f6d6e304a6fd5becef8
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2024-02-25 10:02:43 +0500

    update comments

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
+# Import necessary modules
 from weatherapp import get_weather_data, get_forecast_astro_data
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 import os
 import requests
 
+# Initialize Flask app
 app = Flask(__name__, template_folder="weatherapp/templates")
-CORS(app)
+CORS(app)  # Enable CORS
 
+# Get API key from environment variables
 MAPS_API_KEY = OPENCAGE_API_KEY = os.getenv("MAPS_API_KEY")
 
 
 @app.route("/")
 def index():
+    # Render the index page
     return render_template("index.html")
 
 
 @app.route("/submit", methods=["POST"])
 def submit():
-
+    # Get location from form data
     query_location = request.form["place_name"].lower()
+    # Get weather data for the location
     weather_data = get_weather_data(query=query_location)
     if "error" in weather_data:
         context = {
             "weather_data": weather_data,
             "location": query_location,
         }
+        # If there's an error, render the error page
         if weather_data.get("error")["code"] == 1006:
             return render_template("404.html", **context)
     else:
+        # Get forecast and astronomical data for the location
         forecast_astro_data = get_forecast_astro_data(query=query_location, days=1)
         context = {
             "weather_data": weather_data,
@@ -35,12 +42,13 @@ def submit():
             "weather_condition": weather_data["current"]["condition"]["text"],
             "location": query_location,
         }
-
+        # Render the submit page with the weather and astronomical data
         return render_template("submit.html", **context)
 
 
 @app.route("/api/autocomplete", methods=["GET"])
 def autocomplete():
+    # Get query parameter for autocomplete
     query = request.args.get("q")
 
     if query:
@@ -56,16 +64,14 @@ def autocomplete():
             # Customize the suggestion based on your needs
             suggestion = {
                 "place": result.get("formatted"),
-                # "latitude": result.get("geometry", {}).get("lat"),
-                # "longitude": result.get("geometry", {}).get("lng"),
             }
             suggestions.append(suggestion)
         return jsonify({"suggestions": suggestions})
 
+    # Return error if query parameter is missing
     return jsonify({"error": "Missing query parameter"}), 400
 
 
-# 'error': {'code': 1006, 'message': 'No matching location found.'}}
-
 if __name__ == "__main__":
+    # Run the app
     app.run(debug=True)
